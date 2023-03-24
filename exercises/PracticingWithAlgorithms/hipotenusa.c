10671bd2622faf82be741844499e562901961349
Conde Zarco <ines.conde.zarco@gmail.com>
2023-03-24 17:26:23 -0400

    commit

--- exercises/PracticingWithAlgorithms/hipotenusa.c
+++ exercises/PracticingWithAlgorithms/hipotenusa.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+
  int main ()
  {
- double a, b, hipotenusa;
- printf("introducir valor cateto 1: \n");
- scanf("%lf",&a);
- printf("introducir valor de cateto 2: \n");
- scanf("%lf", &b);
- hipotenusa = a+b;
- printf("hipotenusa es: %lf", hipotenusa);
+    double a, b, hipotenusa, power;
+    power = 2;
+    printf("introducir valor cateto 1: \n");
+    scanf("%lf",&a);
+    printf("introducir valor de cateto 2: \n");
+    scanf("%lf", &b);
+    hipotenusa = sqrt(pow(a, power)) + sqrt(pow(b, power));
+    printf("hipotenusa es: %lf", hipotenusa);
  }
