0f3148e021cdd8aa64712c8acb1e4c7214b17b8c
Conde Zarco <ines.conde.zarco@gmail.com>
2023-03-30 09:01:56 -0400

    commit

--- exercises/PracticingWithAlgorithms/hipotenusa.c
+++ exercises/PracticingWithAlgorithms/hipotenusa.c
@@ -9,6 +9,6 @@
     scanf("%lf",&a);
     printf("introducir valor de cateto 2: \n");
     scanf("%lf", &b);
-    hipotenusa = sqrt(pow(a, power)) + sqrt(pow(b, power));
-    printf("hipotenusa es: %lf", hipotenusa);
+    hipotenusa = sqrt((pow(a, power)) + (pow(b, power)));
+    printf("hipotenusa es: %.2f", hipotenusa);
  }
