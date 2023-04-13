021b0d00d7eaa3621f44290f07bd5725009a9e4f
Conde Zarco <ines.conde.zarco@gmail.com>
2023-04-13 16:31:13 -0400

    commit

---
+++ exercises/PracticingWithAlgorithms/CalcPromedio.c
@@ -0,0 +1,29 @@
+//Desarrolle un algoritmo que permita calcular Promedio de Notas; finaliza cuando N = 0.
+#include <stdio.h>
+
+int main(){
+    int cant;
+    int num = 1;
+    int contador= 0;
+    float suma = 0;
+    float promedio;
+    
+    printf("Ingresar cantidad de notas a promediar: ");
+    scanf("%i", &cant);
+
+    int nota[cant];
+
+    for (int i = 0; i < cant; i++)
+    {
+        printf("Ingresar nota(%i):", num);
+        scanf ("%i", &nota[i]);
+
+        contador = contador+1;
+        num = num +1;
+
+        suma = suma + nota[i];
+    }
+
+    promedio = suma/contador; 
+    printf("El promedio es:%.2f", promedio);
+}
